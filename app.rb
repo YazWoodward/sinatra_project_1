09b1cf77dcdd1e313d7a891628ae735c100491b6
YazWoodward <yazminca.woodward@jesus.ox.ac.uk>
2013-07-29 22:12:25 +0100

    3 variations

--- app.rb
+++ app.rb
@@ -8,3 +8,21 @@ get '/:name/tea' do
 	 @name = params[:name]
     erb :tea
 end
+
+get '/' do 
+	erb :sugar
+end
+
+get '/:name/sugar' do
+	 @name = params[:name]
+    erb :sugar
+end
+
+get '/' do 
+	erb :milk
+end
+
+get '/:name/milk' do
+	 @name = params[:name]
+    erb :milk
+end
